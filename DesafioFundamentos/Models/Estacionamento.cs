0d7f398558af99aba9f1613785fdbe9bd9314e0e
larissa-rodriguesc <151876679+larissa-rodriguesc@users.noreply.github.com>
2024-03-06 22:19:17 -0300

    Update Estacionamento.cs

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,7 +14,7 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-        //TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veículos"
+        // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veículos"
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             string placa = Console.ReadLine();
 
@@ -26,21 +26,21 @@ namespace DesafioFundamentos.Models
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            //Pedir para o usuário digitar a placa e armazenar na variável placa
+            // Pedir para o usuário digitar a placa e armazenar na variável placa
             string placa = Console.ReadLine();
 
-            //Verifica se o veículo existe
+            // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
                 
-                //TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                //TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal
+                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal
                 int horas = Convert.ToInt32(Console.ReadLine());
 
                 decimal valorTotal = precoInicial + precoPorHora * horas;
 
-                //TODO: Remover a placa digitada da lista de veículos
+                // TODO: Remover a placa digitada da lista de veículos
                 veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
